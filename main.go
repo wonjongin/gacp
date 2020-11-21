4a4894d885728617713648b981399c91d04023c3
wonjongin <wji0227@naver.com>
2020-11-21 19:03:43 +0900

    환영메시지,

--- main.go
+++ main.go
@@ -5,10 +5,19 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+
+	. "github.com/logrusorgru/aurora"
 )
 
 func main() {
-	fmt.Println("Hello!")
+	fmt.Println(`
+    __ _   __ _   ___  _ __  
+   / _' | / _' | / __|| '_ \ 
+  | (_| || (_| || (__ | |_) |
+   \__, | \__,_| \___|| .__/ 
+    __/ |             | |    
+   |___/              |_|    
+  `)
 	locGitNum := 0
 	locGitPath := ".git"
 	pwd, _ := os.Getwd()
@@ -32,27 +41,33 @@ func main() {
 	cmdGitAddOut, cmdGitAddErr := cmdGitAdd.Output()
 	if cmdGitAddErr != nil {
 		fmt.Println(cmdGitAddErr)
-		fmt.Println("add 에러")
+		fmt.Println(Red("add 에러"))
 	} else {
 		fmt.Println(string(cmdGitAddOut))
+		fmt.Println(Green("add 완료"))
 	}
 
-	fmt.Printf("커밋메시지: ")
+	fmt.Printf("커밋메시지(취소:q): ")
 	fmt.Scanln(&commitMsg)
+	if commitMsg == "q" {
+		os.Exit(0)
+	}
 	cmdGitCommit := exec.Command("git", "commit", "-m", commitMsg)
 	cmdGitCommitOut, cmdGitCommitErr := cmdGitCommit.Output()
 	if cmdGitCommitErr != nil {
 		fmt.Println(cmdGitCommitErr)
-		fmt.Println("commit 에러")
+		fmt.Println(Red("commit 에러"))
 	} else {
 		fmt.Println(string(cmdGitCommitOut))
+		fmt.Println(Green("commit 완료"))
 	}
 	cmdGitPush := exec.Command("git", "push")
 	cmdGitPushOut, cmdGitPushErr := cmdGitPush.Output()
 	if cmdGitPushErr != nil {
 		fmt.Println(cmdGitPushErr)
-		fmt.Println("push 에러")
+		fmt.Println(Red("push 에러"))
 	} else {
 		fmt.Println(string(cmdGitPushOut))
+		fmt.Println(Green("push 완료"))
 	}
 }
